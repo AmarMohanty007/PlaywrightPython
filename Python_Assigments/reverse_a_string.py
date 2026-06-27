120de5bf81f20168b4126263dbbfb78db42afc78
Amar <amarkumarmohanty216@gmail.com>
2026-06-27 16:01:02 +0530

    API Testing Changes

--- Python_Assigments/reverse_a_string.py
+++ Python_Assigments/reverse_a_string.py
@@ -1,3 +1,4 @@
+from os import remove
 
 # # --------------------------------------------------------------------
 # # write a Python program to reverse a string using looping statement
@@ -5,12 +6,46 @@
 
 
 # Method 1: Using a loop
-actual_text = 'India is my country'
+# actual_text = 'India is my country'
+#
+# print("Actual String:",actual_text)
+# reversed_text = ""
+#
+# for ch in actual_text:
+#     reversed_text = ch + reversed_text
+#
+# print("Reversed string:", reversed_text)
 
-print("Actual String:",actual_text)
-reversed_text = ""
+# my_str = 'i am Sunita!!'
+#
+#
+# print("Actual list is =====>",my_str)
+#
+# empty_str = ''
+# target = 'i'
+# count = 0
+# empty_space = ' '
+#
+# # input a string and reverse it
+# # count the no. occurence of alphabet 'i'
+# # remove the empty space from the string
+#
+# for i in my_str:
+#     if target in i:
+#         count=count+1
+#         continue
+#     elif i not in empty_space:
+#         empty_str = i + empty_str
+#         continue
+#
+# print("After Reverse list =====>",empty_str)
+# print("Alphabet i occurence in the string:",count)
 
-for ch in actual_text:
-    reversed_text = ch + reversed_text
 
-print("Reversed string:", reversed_text)
+n = 5
+
+for i in range(n, 0, -1):
+    print(" " * (n - i) + "* " * i)
+
+for i in range(2, n + 1):
+    print(" " * (n - i) + "* " * i)
