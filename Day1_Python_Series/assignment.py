d1733cb58efe9361ac7b19712d2003e4ea7f6a0f
Amar <amarkumarmohanty216@gmail.com>
2026-07-05 18:26:06 +0530

    Latest API Testing Changes

---
+++ Day1_Python_Series/assignment.py
@@ -0,0 +1,17 @@
+
+
+
+# num1 = "5"
+# num2 = "3"
+# result = num1 * num2
+# print(num1)
+# print(num2)
+# print(result)
+
+# print(10 > 5 and 5 < 3)
+
+# print((4+5)*5**(4-2));
+
+# print(oct(23) + oct(23))
+
+print(~(~2))
